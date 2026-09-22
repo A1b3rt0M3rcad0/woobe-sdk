c9c6e5613a1b8e68ea241934a196c5603f425076
Alberto S. Mercado <109180196+A1b3rt0M3rcad0@users.noreply.github.com>
2026-09-21 21:10:02 -0300

    feat: export runtime contract validation types

--- src/woobe/__init__.py
+++ src/woobe/__init__.py
@@ -1,10 +1,21 @@
 from woobe.client import Woobe
-from woobe.contracts import OutputContextIssue, OutputContextValidation
+from woobe.contracts import (
+    ExternalContextValidation,
+    OutputContextIssue,
+    OutputContextValidation,
+    RuntimeContractIssue,
+    RuntimeContractValidation,
+    RuntimeContractsValidation,
+)
 from woobe.events import WoobeEvent
 
 __all__ = [
+    "ExternalContextValidation",
     "OutputContextIssue",
     "OutputContextValidation",
+    "RuntimeContractIssue",
+    "RuntimeContractValidation",
+    "RuntimeContractsValidation",
     "Woobe",
     "WoobeEvent",
 ]
